60bea8ffccb62c68adacec4a23c30d512ffb891d
Jin <hellothisisjin@gmail.com>
2023-03-16 09:46:44 +0900

    Add every digit

--- WebProgramming/FrontEnd/JavaScript/01_Vanilla_Javascript/Assign_3_Programmers/SumofDigits.js
+++ WebProgramming/FrontEnd/JavaScript/01_Vanilla_Javascript/Assign_3_Programmers/SumofDigits.js
@@ -1,1 +1,14 @@
 /* https://school.programmers.co.kr/learn/courses/30/lessons/12931?language=javascript */
+function solution(n) {
+  var answer = 0;
+  for (let i = 1; i <= 9; ++i) {
+    let curDigit = (n % 10 ** i) / 10 ** (i - 1);
+    answer += curDigit;
+
+    n -= curDigit * 10 ** (i - 1);
+    if (n === 0) {
+      break;
+    }
+  }
+  return answer;
+}
